a208cf35c3eb29a69054e08931921d92f04ab5c2
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-05-06 14:23:58 +0200

    Handle select all in AvaloniaEdit on browser

--- src/XamlPlayground/Behaviors/TextEditorBehavior.cs
+++ src/XamlPlayground/Behaviors/TextEditorBehavior.cs
@@ -1,8 +1,8 @@
 ﻿using Avalonia;
+using Avalonia.Input;
 using Avalonia.Xaml.Interactivity;
 using AvaloniaEdit;
 using AvaloniaEdit.Highlighting;
-using Microsoft.Win32;
 
 namespace XamlPlayground.Behaviors;
 
@@ -12,7 +12,6 @@ public class TextEditorBehavior : Behavior<TextEditor>
         AvaloniaProperty.Register<TextEditorBehavior, string?>(nameof(Extension));
 
     private TextEditor? _textEditor;
-    private RegistryOptions? _registryOptions;
 
     public string? Extension
     {
@@ -32,7 +31,40 @@ public class TextEditorBehavior : Behavior<TextEditor>
         _textEditor = textEditor;
 
         _textEditor.TextArea.SelectionCornerRadius = 0;
+        _textEditor.TextArea.KeyDown += TextAreaOnKeyDown;
 
         _textEditor.SyntaxHighlighting = HighlightingManager.Instance.GetDefinitionByExtension(Extension);
+    }
+
+    protected override void OnDetaching()
+    {
+        if (_textEditor is { } textEditor)
+        {
+            textEditor.TextArea.KeyDown -= TextAreaOnKeyDown;
+        }
+
+        _textEditor = null;
+
+        base.OnDetaching();
+    }
+
+    private void TextAreaOnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (_textEditor is null || e.Key != Key.A)
+        {
+            return;
+        }
+
+        var modifiers = e.KeyModifiers;
+        var isSelectAllGesture = modifiers.HasFlag(KeyModifiers.Control) || modifiers.HasFlag(KeyModifiers.Meta);
+        var hasOtherModifiers = (modifiers & ~(KeyModifiers.Control | KeyModifiers.Meta)) != 0;
+
+        if (!isSelectAllGesture || hasOtherModifiers)
+        {
+            return;
+        }
+
+        _textEditor.SelectAll();
+        e.Handled = true;
     }
 }
